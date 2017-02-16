42e2276f1471b6f2ba82d290d3a29c9cc17af9b1
blurbyte <info@blurbyte.com>
2017-02-16 22:18:54 +0100

    Currency name and current currency formatting

---
+++ src/containers/CurrencyDetails/CurrentCurrencyCode.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const CurrentCurrencyCode = styled.span`
+  font-size: 1.6rem;
+  padding-left: 0.4rem;
+`;
+
+export default CurrentCurrencyCode;
--- src/containers/CurrencyDetails/DetailedCurrencyItem.js
+++ src/containers/CurrencyDetails/DetailedCurrencyItem.js
@@ -3,14 +3,21 @@ import React, { PropTypes } from 'react';
 import StyledCurrencyItem from './StyledCurrencyItem';
 import TextLine from './TextLine';
 import CloseButton from './CloseButton';
+import CurrentCurrencyCode from './CurrentCurrencyCode';
 
 const DetailedCurrencyItem = ({details, handleItemRemove}) => {
+  // shortening currency name if it is too long
+  const CHAR_LIMIT = 24;
+
   return (
     <StyledCurrencyItem>
       <CloseButton onClick={() => handleItemRemove(details.code)} />
       <TextLine code>{details.code}</TextLine>
-      <TextLine name>{details.currency}</TextLine>
-      <TextLine exchange>{details.mid}</TextLine>
+      <TextLine name>{details.currency.length < CHAR_LIMIT ? details.currency : `${details.currency.slice(0, CHAR_LIMIT)}...)`}</TextLine>
+      <TextLine exchange>
+        {details.mid}
+        <CurrentCurrencyCode>{'PLN'}</CurrentCurrencyCode>
+      </TextLine>
     </StyledCurrencyItem>
   );
 };
